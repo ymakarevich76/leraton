74bd0202d8b406386d0da327299f70daba249142
AlexKa <x182838@gmail.com>
2021-10-07 12:09:26 +0300

    bug fix open menu

--- _src/static/script/block/__menu.js
+++ _src/static/script/block/__menu.js
@@ -1,3 +1,36 @@
+const hamburger = document.querySelector('.header__icon-menu');
+const nav = document.querySelector('.header__menu');
+const btnClose = document.querySelector('.header__icon-close');
+const body = document.querySelector('body');
+
+const openMenu = () => {
+  body.classList.add('fixed');
+  nav.classList.add('header__menu--active');
+  hamburger.classList.add('icon__menu--active');
+};
+
+const closeMenu = () => {
+  body.classList.remove('fixed');
+  nav.classList.remove('header__menu--active');
+  hamburger.classList.remove('icon__menu--active');
+}
+
+hamburger.addEventListener('click', () => {
+  openMenu();
+});
+
+btnClose.addEventListener('click', () => {
+  closeMenu();
+});
+
+nav.addEventListener('click', (evt) => {
+  if (evt.target === nav) {
+    closeMenu();
+  }
+});
+
+
+
 if (document.querySelectorAll('.btn-chevron')) {
   const btnsChevron = document.querySelectorAll('.btn-chevron');
   btnsChevron.forEach((btn) => {
